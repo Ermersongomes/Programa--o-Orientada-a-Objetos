afc9c33a9d379382f7fa8e2729a2331c3877e609
Ermerson Gomes <ermersonwwe@gmail.com>
2024-11-01 10:27:58 -0300

    new update
    exercício Controle de finanças trimestral

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author 20241198060006
+ */
+public class JavaApplication5 
+{
+
+    public static void main(String[] args) 
+    {
+        double janeiro = 3500.25;
+        double fevereiro = 2489.57;
+        double marco = 3011.52;
+        
+        System.out.println("Janeiro = " + janeiro);
+        System.out.println("Fevereiro = " + fevereiro);
+        System.out.println("Março = " + marco);
+        
+         for (int j = 0; j < 50; j++) 
+         {
+            System.out.print("_"); // Usa print para ficar na mesma linha
+         }
+       
+        
+        double media = (janeiro + fevereiro + marco) / 3;
+        System.out.println("Média = " + String.format("%.2f", media) + "\n");
+    }
+    
+}
