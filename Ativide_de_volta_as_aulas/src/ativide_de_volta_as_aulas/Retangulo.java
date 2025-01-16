4b76198574b6f3d3b1403333a0cb81efbdbd2d73
Ermerson Gomes <ermersonwwe@gmail.com>
2025-01-16 10:23:18 -0300

    Update Retangulo.java

--- Ativide_de_volta_as_aulas/src/ativide_de_volta_as_aulas/Retangulo.java
+++ Ativide_de_volta_as_aulas/src/ativide_de_volta_as_aulas/Retangulo.java
@@ -9,5 +9,38 @@ package ativide_de_volta_as_aulas;
  * @author 20241198060006
  */
 public class Retangulo {
+    private double largura;
+    private double altura; 
     
+    public Retangulo( double largura, double altura){
+        this.largura = largura;
+        this.altura = altura;
+    }
+    
+    public double getLargura(){
+        return largura;
+    }
+    public double getAltura(){
+        return altura;
+    }
+   //......................................
+    
+    public void setLargura(double largura){
+        this.largura = largura;
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    //.....................................
+    
+    public static void calcularArea (double area,  double largura, double altura){
+        
+        area = largura * altura;
+        
+    }
+    
+    public static void main(String[] args){
+        calcularArea (5, 10);
+        
+    }
 }
